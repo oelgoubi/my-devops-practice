29b3c13a15be4dec5e376ae04dc92749fa09d8c2
othmane.elgoubi <othmanelgoubi@gmail.com>
2024-04-03 16:09:32 +0200

    infra: add s3 remote backend

--- terraform-practice/eks/0-provider.tf
+++ terraform-practice/eks/0-provider.tf
@@ -1,14 +1,14 @@
 terraform {
 
-  # backend "s3" {
-  #   bucket         = "devops-directive-tf-state-othmane"
-  #   key            = "playground/eks-cluster/terraform.tfstate"
-  #   region         = "us-east-1"
-  #   dynamodb_table = "terraform-state-locking"
-  #   encrypt        = true
-  #   access_key     = "AKIA3TXIKLA2N65NU5JH" # Access key and Secret key for the user that has permissions
-  #   secret_key     = "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"
-  # }
+  backend "s3" {
+    bucket         = "devops-directive-tf-state-othmane"
+    key            = "playground/eks-cluster/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+    access_key     = "" # Access key and Secret key for the user that has permissions
+    secret_key     = ""
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
