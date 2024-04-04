a556615337e0f05981e0485129287a62725f16d1
othmane.elgoubi <othmanelgoubi@gmail.com>
2024-04-04 23:34:51 +0200

    feat: install first argocd app

--- terraform-practice/eks/argocd.tf
+++ terraform-practice/eks/argocd.tf
@@ -31,11 +31,3 @@ resource "null_resource" "del-argo-pass" {
     command = "kubectl -n argocd delete secret argocd-initial-admin-secret"
   }
 }
-
-#  Get new password : "password"
-#  bcrypt(password)=$2a$10$rRyBsGSHK6.uc8fntPwVIuLVHgsAhAX7TcdrqW/RADU0uh7CaChLa
-#  kubectl -n argocd patch secret argocd-secret \
-#   -p '{"stringData": {
-#     "admin.password": "$2a$10$rRyBsGSHK6.uc8fntPwVIuLVHgsAhAX7TcdrqW/RADU0uh7CaChLa",
-#     "admin.passwordMtime": "'$(date +%FT%T%Z)'"
-#   }}
